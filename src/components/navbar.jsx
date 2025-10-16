441f765e962b825f1041c510409debc7f2cb2e6b
DHRUBOJYOTI BHATTACHARJEE <117100651+dhrubooou@users.noreply.github.com>
2025-10-16 21:04:55 +0530

    Initial commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,36 @@
-import React from "react";
-
+import React, { useState } from "react"; 
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <div className="logo left permanent-marker-regular" style={{fontFamily:"UnifrakturMaguntia"}}><code>Dhrubojyoti's Portfolio</code></div>
-      <div className="right">
+      {/* Logo */}
+      <div
+        className="logo left permanent-marker-regular"
+        style={{ fontFamily: "UnifrakturMaguntia" }}
+      >
+        <code>Dhrubojyoti's Portfolio</code>
+      </div>
+
+      {/* Hamburger Icon */}
+      <div
+        className="hamburger"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Navigation Links */}
+      <div className={`right ${isOpen ? "open" : ""}`}>
         <ul>
-            <li style={{ fontSize:20 }}><a href="#home">Home</a></li>
-            <li style={{ fontSize:20 }}><a href="#about">About</a></li>
-            <li style={{ fontSize:20 }}><a href="#projects">Projects</a></li>
-            <li style={{ fontSize:20 }}><a href="#techstack">Tech Stack</a></li>
-            <li style={{ fontSize:20 }}><a href="#contactme">Contact Me</a></li>
-
-
+          <li style={{ fontSize: 20 }}><a href="#home">Home</a></li>
+          <li style={{ fontSize: 20 }}><a href="#about">About</a></li>
+          <li style={{ fontSize: 20 }}><a href="#projects">Projects</a></li>
+          <li style={{ fontSize: 20 }}><a href="#techstack">Tech Stack</a></li>
+          <li style={{ fontSize: 20 }}><a href="#contactme">Contact Me</a></li>
         </ul>
       </div>
     </nav>
--- src/components/section.jsx
+++ src/components/section.jsx
@@ -35,7 +35,7 @@ function Sec() {
         <div> and I am a Passionate </div>
         <div id="element"><code>{MyComponent()}</code></div>
         <div className="profile-buttons">
-          <a href="/Dhrubojyoti_Bhattacharjee_SDE_CV.pdf" download className="icon-btn border-only">
+          <a href="https://drive.google.com/file/d/1wrz0SHlxtZLn7dvOzQqwGNHiQ8aaGjUR/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="icon-btn border-only">
             <img src={ resume } alt="Resume" />
             My Resume
           </a>
@@ -52,7 +52,7 @@ function Sec() {
       </div>
       <div className="rightSection">
         <div className="orbitContainer">
-          <img src="https://media.licdn.com/dms/image/v2/D5603AQEt5eQZpuVItQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1706942512495?e=1757548800&v=beta&t=woK3TZX7QZhGJFmifhEh3NJjokjLJwAMg1Zl2xAT2V8" alt="Dhrubo's_Profile_picture" className="pfp" />
+          <img src="https://media.licdn.com/dms/image/v2/D5603AQFMK8KRdMAyNA/profile-displayphoto-crop_800_800/B56ZlcJQzsKEAM-/0/1758187557559?e=1761782400&v=beta&t=dpjl4gzTdTJfphFcqaWr_eXY1nmnldv3NkelTq-6KGs" alt="Dhrubo's_Profile_picture" className="pfp" />
         </div>
       </div>
 
